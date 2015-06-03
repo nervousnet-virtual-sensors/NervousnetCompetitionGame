d820eb1261800a419417d8f6fbe1108b9c7e93d0
Prasad79 <prasad.pulikal@students.fhnw.ch>
2015-06-03 09:17:24 +0200

    changes to correct graph for team values and new GameServer.jar

--- GameServer/src/Competition.java
+++ GameServer/src/Competition.java
@@ -2,7 +2,11 @@ import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.ListIterator;
 
 import com.example.nervousco.AccReading;
 import com.example.nervousco.LightReading;
@@ -11,10 +15,10 @@ import com.example.nervousco.Reading;
 public class Competition {
 
 	public final static int COMPETITION_TYPE_ACCELEROMETER = 0;
-	public final static int COMPETITION_TYPE_LIGHT= 1;
-	
+	public final static int COMPETITION_TYPE_LIGHT = 1;
+
 	public int COMPETITION_TYPE = COMPETITION_TYPE_ACCELEROMETER;
-	
+
 	double[] team = new double[] { 0, 0 };
 	ArrayList<Reading> readings = new ArrayList<Reading>();
 	AccReading[] lastReadings = new AccReading[] {
@@ -23,32 +27,47 @@ public class Competition {
 	LightReading[] lastLightReadings = new LightReading[] {
 			new LightReading(0, 0, 0), new LightReading(0, 0, 0) };
 
+	Hashtable<String, Reading> hUniquePlayersRedTeam = new Hashtable<String, Reading>(); // key is the android_id
+														// and value is the last
+														// reading
+	Hashtable<String, Reading> hUniquePlayersGreenTeam = new Hashtable<String, Reading>();
+
 	public synchronized void reset() {
 		team = new double[] { 0, 0 };
 		readings.clear();
-		lastReadings =  new AccReading[] {
-				new AccReading(0, 0, 0, 0, 0), new AccReading(0, 0, 0, 0, 1) };
-		lastLightReadings = new LightReading[] {
-				new LightReading(0, 0, 0), new LightReading(0, 0, 0) };
+		hUniquePlayersRedTeam.clear();
+		hUniquePlayersGreenTeam.clear();
+		lastReadings = new AccReading[] { new AccReading(0, 0, 0, 0, 0),
+				new AccReading(0, 0, 0, 0, 1) };
+		lastLightReadings = new LightReading[] { new LightReading(0, 0, 0),
+				new LightReading(0, 0, 0) };
 	}
 
 	public synchronized void pushReading(AccReading r) {
 		readings.add(r);
-
 		lastReadings[r.team] = r;
 
+		if (r.team == 0) {
+			hUniquePlayersGreenTeam.put(r.android_id, r);
+
+			if (hUniquePlayersRedTeam.contains(r.android_id))
+				hUniquePlayersRedTeam.remove(r.android_id);
+		} else {
+			hUniquePlayersRedTeam.put(r.android_id, r);
+
+			if (hUniquePlayersGreenTeam.contains(r.android_id))
+				hUniquePlayersGreenTeam.remove(r.android_id);
+		}
+
 		int t = r.team;
-//		for (int t = 0; t <= 1; t++) {
-			team[t] += Math.abs(lastReadings[t].x)
-					+ Math.abs(lastReadings[t].y)
-					+ Math.abs(lastReadings[t].z);
-			
-//			System.out.println("Score Team "+t+" - "+team[t]);
-//		}
-		
-		
-		
-//		System.out.println("lastReadings");
+		// for (int t = 0; t <= 1; t++) {
+		team[t] += Math.abs(lastReadings[t].x) + Math.abs(lastReadings[t].y)
+				+ Math.abs(lastReadings[t].z);
+
+		// System.out.println("Score Team "+t+" - "+team[t]);
+		// }
+
+		// System.out.println("lastReadings");
 	}
 
 	public synchronized void pushReading(LightReading r) {
@@ -56,11 +75,25 @@ public class Competition {
 
 		lastLightReadings[r.team] = r;
 
+		
+		if (r.team == 0) {
+			hUniquePlayersGreenTeam.put(r.android_id, r);
+
+			if (hUniquePlayersRedTeam.contains(r.android_id))
+				hUniquePlayersRedTeam.remove(r.android_id);
+		} else {
+			hUniquePlayersRedTeam.put(r.android_id, r);
+
+			if (hUniquePlayersGreenTeam.contains(r.android_id))
+				hUniquePlayersGreenTeam.remove(r.android_id);
+		}
+		
 		int t = r.team;
-//		for (int t = 0; t <= 1; t++) {
-			team[t] += lastLightReadings[t].lightVal;
-//			System.out.println("Score Team "+t+" - "+team[t]);
-//		}
+
+		// for (int t = 0; t <= 1; t++) {
+		team[t] += lastLightReadings[t].lightVal;
+		
+		// }
 	}
 
 	public synchronized double getScore() {
@@ -69,32 +102,94 @@ public class Competition {
 		}
 		return 0.5 + (team[1] - team[0]) / (team[0] + team[1]) / 2;
 	}
-	
+
+	// public synchronized double[] getLastReadings() {
+	// double[] teamScore = new double[] { 0, -0 };
+	// if (COMPETITION_TYPE == COMPETITION_TYPE_ACCELEROMETER){
+	//
+	// for (int t = 0; t <= 1; t++) {
+	// teamScore[t] = Math.abs(lastReadings[t].x)
+	// + Math.abs(lastReadings[t].y)
+	// + Math.abs(lastReadings[t].z);
+	//
+	// if(teamScore[t] == 0)
+	// teamScore[t] = -1;
+	// System.out.println("Team Score Accel "+teamScore[t]);
+	// }
+	//
+	//
+	//
+	// }else {
+	// for (int t = 0; t <= 1; t++) {
+	// teamScore[t] = lastLightReadings[t].lightVal;
+	// if(teamScore[t] == 0)
+	// teamScore[t] = -1;
+	// System.out.println("Team Score Light "+
+	// (t==0?"GREEN":"RED")+" Score =  "+ teamScore[t]);
+	// }
+	//
+	// }
+	// return teamScore;
+	// }
+
 	public synchronized double[] getLastReadings() {
-		double[] teamScore = new double[] { 0, -0 };
-		if (COMPETITION_TYPE == COMPETITION_TYPE_ACCELEROMETER){
+		double[] teamScore = new double[] { 0, 0 };
+		
+		Enumeration<Reading> e = hUniquePlayersGreenTeam.elements();		
+		
+		while(e.hasMoreElements()){
+			Reading r = (Reading) e.nextElement();
 			
-			for (int t = 0; t <= 1; t++) {
-				teamScore[t] = Math.abs(lastReadings[t].x)
-						+ Math.abs(lastReadings[t].y)
-						+ Math.abs(lastReadings[t].z);
-				
-				if(teamScore[t] == 0)
-					teamScore[t] = -1;
-				System.out.println("Team Score Accel "+teamScore[t]);
+			if(COMPETITION_TYPE == COMPETITION_TYPE_ACCELEROMETER){
+				if(r instanceof AccReading){
+					AccReading accR = (AccReading) r;
+					
+					if(accR.team == 0)
+					teamScore[0] += Math.abs(accR.x) + Math.abs(accR.y)
+							+ Math.abs(accR.z);
+					
+				}
+			}else if(COMPETITION_TYPE == COMPETITION_TYPE_LIGHT){
+				if(r instanceof LightReading){
+					LightReading lightR = (LightReading) r;
+					
+					if(lightR.team == 0)
+					teamScore[0] += lightR.lightVal;
+					
+				}
 			}
-			
-			
-			
-		}else {
-			for (int t = 0; t <= 1; t++) {
-				teamScore[t] = lastLightReadings[t].lightVal;
-				if(teamScore[t] == 0)
-					teamScore[t] = -1;
-				System.out.println("Team Score Light "+ (t==0?"GREEN":"RED")+" Score =  "+ teamScore[t]);
-			}
-			
+		
 		}
+		
+		
+Enumeration<Reading> e2 = hUniquePlayersRedTeam.elements();		
+		
+		while(e2.hasMoreElements()){
+			Reading r = (Reading) e2.nextElement();
+			
+			if(COMPETITION_TYPE == COMPETITION_TYPE_ACCELEROMETER){
+				if(r instanceof AccReading){
+					AccReading accR = (AccReading) r;
+					
+					if(accR.team == 1)
+					teamScore[1] += Math.abs(accR.x) + Math.abs(accR.y)
+							+ Math.abs(accR.z);
+					
+				}
+			}else if(COMPETITION_TYPE == COMPETITION_TYPE_LIGHT){
+				if(r instanceof LightReading){
+					LightReading lightR = (LightReading) r;
+					
+					if(lightR.team == 1)
+					teamScore[1] += lightR.lightVal;
+					
+				}
+			}
+		
+		}
+
+		
+		System.out.println("GREEEN TEAM -- "+teamScore[0]+", RED TEAM -- "+teamScore[1]);
 		return teamScore;
 	}
 
--- GameServer/src/com/example/nervousco/Reading.java
+++ GameServer/src/com/example/nervousco/Reading.java
@@ -5,4 +5,8 @@ import java.io.Serializable;
 public class Reading implements Serializable {
 	public long serialVersionUID;
 	public String android_id; 
+	
+	
+	
+	
 }
